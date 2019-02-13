b4f15bc4dc59ab0320c1213a9e44c4f6e33b0478
Saul <cs_clavijo@hotmail.com>
2019-02-12 22:58:09 -0300

    Se agregan migraciones basicas

---
+++ database/migrations/2019_02_13_014744_create_menu_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenuItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // @TODO: Un menu item pertenece a un menu
+        // y si pertenece a un menu item es un menu desplegable (?)
+        Schema::create('menu_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu_items');
+    }
+}
