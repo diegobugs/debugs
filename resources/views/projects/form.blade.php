3c77792be313be8cfe9aef80ad1e1bbe247e1641
Saul Clavijo <saul@oktana.io>
2019-02-19 11:30:58 -0300

    Se agregan columnas faltantes del proyecto en el formulario y en el controlador

--- resources/views/projects/form.blade.php
+++ resources/views/projects/form.blade.php
@@ -9,6 +9,14 @@
         {!! Form::label('name', 'Name') !!}
         {{ Form::text('name', Input::old('name'), ['class' => 'form-control', 'placeholder' => 'Name']) }}
     </div>
+    <div class="form-group">
+        {!! Form::label('skey', 'Shortcut key') !!}
+        {{ Form::text('skey', Input::old('skey'), ['class' => 'form-control', 'placeholder' => 'Shortcut key']) }}
+    </div>
+    <div class="form-group">
+        {!! Form::label('description', 'Description') !!}
+        {!! Form::textarea('description', Input::old('description'), ['class' => 'form-control', 'placeholder' => 'Description of the project']) !!}
+    </div>
 @if($route != 'show')
     <hr />
     {{ Form::submit('Save', ['name' => 'submit', 'class' => 'btn btn-primary']) }}
