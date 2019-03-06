9c2739d1a6769b694d1a205634e09e0bff026df6
Saul Clavijo F <cs_clavijo@hotmail.com>
2019-03-06 11:34:31 -0300

    Avances

--- app/Debugs/DynamicForm.php
+++ app/Debugs/DynamicForm.php
@@ -12,6 +12,8 @@ class DynamicForm
 
     private static $method;
 
+    private static $form = '';
+
     private static $methods = array('create' => 'POST', 'edit' => 'PATCH', 'show' => 'GET');
 
     public static function generateForm($model, String $method)
@@ -27,40 +29,60 @@ class DynamicForm
         // TODO: Agregar Labels
         // TODO: Decidir como mostrar lo que se puede validar o no
         // TODO: Agregar los span para errores
-        // TODO: Agregar divs para que se vea bien el input (div.form-group en bootstrap)
-        $form = '';
-        if (static::$method == 'view' ){
-            $form .= Form::model(static::$model);
-            $form .= '<fieldset disabled>';
-        } else {
-            $form .= Form::model(static::$model ,['method ' => static::$methods[static::$method]]);
-        }
-        foreach (static::getObject()->fields as $field) {
-            $fieldName = $field->name;
-            $form .= '<div class="form-group">';
-            $form .= Form::label($field->name, $field->label);
-            $form .= Form::input($field->type, $field->name, Input::old($field->name, static::$model->$fieldName), ['class' =>'form-control']) . PHP_EOL;
-            $form .= '</div>';
-        }
-        if (static::$method == 'view' ){
-            $form .= '</fieldset>';
-        } else {
-            $form .= '<hr />';
-            $form .= Form::submit('Save', ['name' => 'submit', 'class' => 'btn btn-primary']);
-        }
-        $form .= Form::close();
+        // TODO: Agregar divs para que se vea bien el input (div.form-group en bootstrap
 
-        return $form;
+        static::openForm();
+        static::generateInputs();
+        static::closeForm();
+
+        return static::$form;
     }
 
     private static function edit()
-    { }
+    {
+        static::openForm();
+        static::generateInputs();
+        static::closeForm();
+    }
 
     private static function view()
-    { }
+    {
+        static::$form .= Form::model(static::$model);
+        static::$form .='<fieldset disabled>';
+
+        static::generateInputs();
+        static::closeForm();
+    }
 
     private static function getObject()
     {
         return Obj::where('class_name', class_basename(static::$model))->first();
+    }
+
+    private static function openForm()
+    {
+        static::$form .= Form::model(static::$model, ['method ' => static::$methods[static::$method]]);
+    }
+
+    private static function generateInputs()
+    {
+        foreach (static::getObject()->fields()->whereNotIn('name', static::$model->getHidden())->get() as $field) {
+            $fieldName = $field->name;
+            static::$form .= '<div class="form-group">';
+            static::$form .= Form::label($field->name, $field->label);
+            static::$form .= Form::input($field->type, $field->name, Input::old($field->name, static::$model->$fieldName), ['class' =>' form-control']) . PHP_EOL;
+            static::$form .= '</div>';
+        }
+        if (static::$method == 'view'){ 
+            static::$form .= '</fieldset>';
+        } else {
+            static::$form .= '<hr />';
+            static::$form .= Form::submit('Save', ['name' => 'submit', 'class' => 'btn btn-primary']);
+        }
+    }
+
+    private static function closeForm()
+    {
+        static::$form .= Form::close();
     }
 }
